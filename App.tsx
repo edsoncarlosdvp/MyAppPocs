e5d0dde76762680155f46660e0696dfc3e31178a
edsoncarlosdvp <edsoncarlos@ymail.com>
2022-05-18 16:25:46 -0300

    Funcionando

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import {Routes} from './src/Routes';
 
 export default function App() {
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,5 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import 'react-native-gesture-handler';
 import {DrawerRoutes} from './Drawer.routes';
 
 export function Routes() {
